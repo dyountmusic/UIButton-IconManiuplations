af56272f5221308b74f79c48826e45859c63ed73
Daniel Yount <yountdaniel@gmail.com>
2019-11-20 18:41:03 -0700

    initial checkin

---
+++ ButtonWithIcon/RoundedOutlineButton.swift
@@ -0,0 +1,48 @@
+//
+//  RoundedOutlineButton.swift
+//  ButtonWithIcon
+//
+//  Created by Daniel Yount on 11/20/19.
+//  Copyright © 2019 Daniel Yount. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundedOutlineButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        applyButtonStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        applyButtonStyle()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        applyButtonStyle()
+    }
+    
+}
+
+extension RoundedOutlineButton {
+    func applyButtonStyle() {
+        backgroundColor = .clear
+        
+        setTitleColor(.black, for: .normal)
+        setTitleColor(.darkGray, for: .highlighted)
+        setTitle("Press Me!", for: .normal)
+        
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 1
+        
+        layer.cornerRadius = 7
+        layer.cornerCurve = .continuous
+        
+        imageView?.contentMode = .scaleAspectFit
+        
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 0)
+        imageEdgeInsets = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    }
+}
---
+++ ButtonWithIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ButtonWithIcon
+//
+//  Created by Daniel Yount on 11/20/19.
+//  Copyright © 2019 Daniel Yount. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
